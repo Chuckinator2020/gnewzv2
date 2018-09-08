1f6e15f4a4d2e59bddca89b3bb5dd83ba9a6694f
Charles F. S. Buffington II <samjobuff@gmail.com>
2018-09-08 01:51:51 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const apiKey = 'a39af26ac61d4465886810a50850f614';
+const defaultSource = 'the-verge';
+const sourceSelector = document.querySelector('#sources');
+const newsArticles = document.querySelector('main');
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () =>
+    navigator.serviceWorker.register('sw.js')
+      .then(registration => console.log('Service Worker registered'))
+      .catch(err => 'SW registration failed'));
+}
+
+window.addEventListener('load', e => {
+  sourceSelector.addEventListener('change', evt => updateNews(evt.target.value));
+  updateNewsSources().then(() => {
+    sourceSelector.value = defaultSource;
+    updateNews();
+  });
+});
+
+window.addEventListener('online', () => updateNews(sourceSelector.value));
+
+window.addEventListener('beforeinstallprompt', e => {
+  btnAdd.style.display = 'block';
+  deferredPrompt.prompt();
+  deferedPrompt.userChoice
+    .then((choiceResult) =>{
+      if (choiceResult.outcome === 'accepted'){
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+
+window.addEventListener('appinstalled', evt => {
+  app.logEvent('a2hs', 'installed');
+});
+
+async function updateNewsSources() {
+  const response = await fetch(`https://newsapi.org/v2/sources?apiKey=${apiKey}`);
+  const json = await response.json();
+  sourceSelector.innerHTML =
+    json.sources
+      .map(source => `<option value="${source.id}">${source.name}</option>`)
+      .join('\n');
+}
+
+async function updateNews(source = defaultSource) {
+  newsArticles.innerHTML = '';
+  const response = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&sortBy=top&apiKey=${apiKey}`);
+  const json = await response.json();
+  newsArticles.innerHTML = json.articles.map(createArticle).join('\n');
+}
+
+function createArticle(article) {
+  return `
+    <div class="article">
+      <a href="${article.url}">
+        <h2>${article.title}</h2>
+        <img src="${article.urlToImage}" alt="${article.title}">
+        <p>${article.description}</p>
+      </a>
+    </div>
+  `;
+}
